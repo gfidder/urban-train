45305b3a0fa10f79a1de677927eb0f670def1d9e
gfidder <gabfid@gmail.com>
2023-02-04 00:38:13 +0000

    adding some base files

---
+++ components/AuthShowcase.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-4">
+    <p class="text-center text-2xl text-white"></p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "My App",
+});
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+    <AuthShowcase />
+  </div>
+</template>
